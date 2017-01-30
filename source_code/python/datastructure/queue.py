aca6c85bf275e9e9650935e2bf9934e75f059b26
ythwork <ythwork83@gmail.com>
2017-01-31 01:34:44 +0900

    stack, queue

---
+++ source_code/python/datastructure/queue.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, data, _next = None):
+        self.data = data
+        self.next = _next
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def IsEmpty(self):
+        if not self.head:
+            return True
+
+        return False
+
+    def Enqueue(self, data):
+        newNode = Node(data)
+        
+        if not self.head:
+            self.head = newNode
+            self.tail = newNode
+            return
+
+        self.tail.next = newNode
+        self.tail = newNode
+
+    def Dequeue(self):
+        if not self.head:
+            print("There is no data")
+            exit(-1)
+
+        retData = self.head.data
+        self.head = self.head.next
+
+        return retData
+
+    def Peek(self):
+        if not self.head:
+            print("There is no data")
+            exit(-1)
+
+        return self.head.data
+
+if __name__ == "__main__":
+    queue = Queue()
+
+    queue.Enqueue(1)
+    queue.Enqueue(2)
+    queue.Enqueue(3)
+    queue.Enqueue(4)
+    queue.Enqueue(5)
+
+    while not queue.IsEmpty():
+        print(queue.Dequeue())
+
+
+    
---
+++ source_code/python/datastructure/stack.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, data, _next = None):
+        self.data = data
+        self.next = _next
+
+class Stack:
+    def __init__(self):
+        #LIFO(Last Input First Out)이므로 맨 앞만 가리키고 있으면 됩니다
+        self.head = None
+
+    def IsEmpty(self):
+        if self.head == None:
+            return True
+
+        return False
+
+    def push(self, data):
+        newNode = Node(data)
+
+        newNode.next = self.head
+        self.head = newNode
+
+    def pop(self):
+        if self.head == None:
+            print("There is no data")
+            exit(-1)
+
+        retData = self.head.data
+
+        self.head = self.head.next
+        return retData
+
+    def peek(self):
+        if self.head == None:
+            print("There is no data")
+            exit(-1)
+
+        return self.head.data
+
+if __name__ == "__main__":
+    stack = Stack()
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+
+    while not stack.IsEmpty():
+        print(stack.pop())
+
+        
