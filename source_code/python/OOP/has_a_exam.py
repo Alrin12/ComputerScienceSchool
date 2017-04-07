6cb8db773ddd9462d26cd2f39fc351243569a8a1
ythwork <ythwork83@gmail.com>
2017-04-07 23:27:50 +0900

    has-a

--- source_code/python/OOP/has_a_exam.py
+++ source_code/python/OOP/has_a_exam.py
@@ -1,17 +1,35 @@
 class Gun:
-    def __init__(self, gun_kind):
-        self.gunkind = gun_kind
-
-class Policeman(Gun):
+    def __init__(self, kind):
+        self.kind = kind
+    
+    def bang(self):
+        print("빵야~빵야")
+        
+#객체합성
+class Police:
     def __init__(self, gun_kind = ''):
-        self.gun = Gun(gun_kind)
-        if not gun_kind:
-            self.gun = None
-
+        if gun_kind:
+            self.gun = Gun(gun_kind)
+        else:
+            self.gun = None  
+            
+    def get_gun(self, gun_kind):
+        self.gun = Gun(gun_kind) 
+    
+    def shoot(self):
+        if self.gun:
+            self.gun.bang()
+        else:
+            print("너 총 없어. 못 쏴!")
+            
 if __name__ == "__main__":
-    police_with_gun = Policeman("리볼버")
-    print(police_with_gun.gun.gunkind)
+    p1 = Police("리볼버")
+    p1.shoot()
 
-    police_without_gun = Policeman()
-    print(police_without_gun.gun)
+    p2 = Police()
+    p2.shoot()
+
+    p2.get_gun("기관총")
+    p2.shoot()
+    
     
