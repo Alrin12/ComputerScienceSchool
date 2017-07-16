10a0d2b6b4fa06e37482c3c61675cb8c6734a57d
ythwork <ythwork83@gmail.com>
2017-07-16 13:41:32 +0900

    modified

--- source_code/python/data_analysis_procedure/class_A_data_analysis_answer.py
+++ source_code/python/data_analysis_procedure/class_A_data_analysis_answer.py
@@ -2,11 +2,32 @@ import pickle
 import math
 from functools import reduce
 
+def get_data(filename, mode = "wt"):
+    f = open(filename, mode)
+    items = []
+
+    while 1:
+        try: 
+            data = pickle.load(f)
+        except EOFError:
+            break    
+        items.append(data)
+
+    scores = []
+    for item in items:
+        for value in item.values():
+            scores.append(value)
+    f.close()
+    return scores
+
 def average(scores):
     return reduce(lambda a, b: a + b, scores)/len(scores)
 
 def variance(scores, avrg):
     return reduce(lambda a, b: a + b, map(lambda s:(s-avrg)**2, scores))/len(scores)
+
+def std_dev(variance):
+    return round(math.sqrt(variance), 1)
 
 def evaluateClass(avrg, std_dev):
     if avrg <50 and std_dev >20:
@@ -18,48 +39,25 @@ def evaluateClass(avrg, std_dev):
     elif avrg > 50 and std_dev <20:
         print("성적도 평균 이상이고 학생들의 실력차도 크지 않다.")
 
-f = open("class_A.bin", "rb")
+if __name__ == "__main__":
+    scores = get_data("class_A.bin", "rb")
+    print(scores)
 
-items = []
+    avrg = average(scores)
+    variance = round(variance(scores, avrg), 1)
+    standard_deviation = std_dev(variance)
+    print("\n")
 
-while 1:
-    try: 
-        data = pickle.load(f)
-    except EOFError:
-        break    
-    items.append(data)
+    print('*' * 50)
+    print("A반 성적 분석 결과")
+    print('*' * 50)
+    print("A반의 평균은 {0}점이고 분산은 {1}이며, 따라서 표준편차는 {2}이다"\
+          .format(avrg, variance, standard_deviation))
+    print('*' * 50)
+    print("A반 종합 평가")
+    print('*' * 50)
+    print('\n')
 
-print(items)
+    evaluateClass(avrg, standard_deviation)
 
-scores = []
 
-# normal way
-'''
-for item in items:
-    for value in item.values():
-        scores.append(value)
-'''
-
-#pythonic way??
-scores = list(map(lambda x: x[0] , map(lambda item:list(item.values()),items)))
-
-print(scores)
-
-avrg = average(scores)
-variance = round(variance(scores, avrg), 1)
-standard_deviation = round(math.sqrt(variance), 1)
-
-print("\n")
-
-print('*' * 50)
-print("A반 성적 분석 결과")
-print('*' * 50)
-print("A반의 평균은 {0}점이고 분산은 {1}이며, 따라서 표준편차는 {2}이다".format(avrg, variance, standard_deviation))
-print('*' * 50)
-print("A반 종합 평가")
-print('*' * 50)
-
-print('\n')
-evaluateClass(avrg, standard_deviation)
-
-f.close()
